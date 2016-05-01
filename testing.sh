724aae4404ef7b5639daa3b260eb51c58b94e352
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 19:33:52 +0000

    wait 10 seconds before trying it

--- testing.sh
+++ testing.sh
@@ -3,9 +3,8 @@
 docker build -t ninthgrimmercury/needlessbeta . &&
     docker build -t freakygamma/needlessbeta test &&
     docker run --interactive --tty  --privileged --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 23628:80 freakygamma/needlessbeta &&
-    OBSERVED=$(mktemp) &&
-    echo ${OBSERVED} &&
-    curl --output ${OBSERVED} http://127.0.0.1:23628/index.html &&
+    sleep 10s &&
+    curl http://127.0.0.1:23628/index.html &&
     docker rmi --force ninthgrimmercury/needlessbeta &&
     docker rmi --force freakygamma/needlessbeta &&
     true
