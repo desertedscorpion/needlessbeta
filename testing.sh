06cbd4bebff19ba1f391834149521ee80432587a
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 19:28:06 +0000

    fixed by using more flags esp privileged

--- testing.sh
+++ testing.sh
@@ -2,7 +2,7 @@
 
 docker build -t ninthgrimmercury/needlessbeta . &&
     docker build -t freakygamma/needlessbeta test &&
-    docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 23628:80 freakygamma/needlessbeta &&
+    docker run --interactive --tty  --privileged --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 23628:80 freakygamma/needlessbeta &&
     OBSERVED=$(mktemp) &&
     echo ${OBSERVED} &&
     curl --output ${OBSERVED} http://127.0.0.1:23628/index.html &&
