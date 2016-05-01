eb332189b80777e56cebba8afaeec6a9feb93ba5
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 18:33:54 +0000

    wtf

--- testing.sh
+++ testing.sh
@@ -2,10 +2,10 @@
 
 docker build -t ninthgrimmercury/needlessbeta . &&
     docker build -t freakygamma/needlessbeta test &&
-    docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 127.0.0.1:23628:80 freakygamma/needlessbeta &&
+    docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 23628:80 freakygamma/needlessbeta &&
     OBSERVED=$(mktemp) &&
-    curl --output ${OBSERVED} http://127.0.0.1:23628/index.html &&
     echo ${OBSERVED} &&
+    curl --output ${OBSERVED} http://127.0.0.1:23628/index.html &&
     docker rmi --force ninthgrimmercury/needlessbeta &&
     docker rmi --force freakygamma/needlessbeta &&
     true
