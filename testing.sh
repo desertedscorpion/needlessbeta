8843b0dc9ebc1312fbe9101bbea869144d62a503
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 18:06:16 +0000

    retry

--- testing.sh
+++ testing.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo this seems straightforward, so just pass it, but if I am wrong, this is where tests should be put.
+docker build -t ninthgrimmercury/needlessbeta . &&
+    docker build -t freakygamma/needlessbeta test &&
+    docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 127.0.0.1.23628:22 freakygamma/needlessbeta &&
+    ssh -i test/id_rsa -p 23628 -l root 127.0.0.1 -o StrictHostKeyChecking=no echo hi &&
+    true
