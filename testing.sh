01dfdbe6d626b1dbb90592d30bd1c49b41e58eac
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 18:29:12 +0000

    echo observed temp file

--- testing.sh
+++ testing.sh
@@ -4,7 +4,8 @@ docker build -t ninthgrimmercury/needlessbeta . &&
     docker build -t freakygamma/needlessbeta test &&
     docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 127.0.0.1:23628:80 freakygamma/needlessbeta &&
     OBSERVED=$(mktemp) &&
-    curl --output ${OBSERVED} http://127.0.0.1:23628/index.html
+    curl --output ${OBSERVED} http://127.0.0.1:23628/index.html &&
+    echo ${OBSERVED} &&
     docker rmi --force ninthgrimmercury/needlessbeta &&
     docker rmi --force freakygamma/needlessbeta &&
     true
