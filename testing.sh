280eda5c831af3763ada8f2d7cb974da5a7bac35
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 18:22:06 +0000

    using httpd not ssh

--- testing.sh
+++ testing.sh
@@ -2,8 +2,9 @@
 
 docker build -t ninthgrimmercury/needlessbeta . &&
     docker build -t freakygamma/needlessbeta test &&
-    docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 127.0.0.1:23628:22 freakygamma/needlessbeta &&
-    ssh -i test/id_rsa -p 23628 -l root 127.0.0.1 -o StrictHostKeyChecking=no echo hi &&
+    docker run --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 127.0.0.1:23628:80 freakygamma/needlessbeta &&
+    OBSERVED=$(mktemp) &&
+    curl --output ${OBSERVED} http://127.0.0.1:23628/index.html
     docker rmi --force ninthgrimmercury/needlessbeta &&
     docker rmi --force freakygamma/needlessbeta &&
     true
