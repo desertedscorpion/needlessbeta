55bb020113c3b6a735cd8bf47228be556752d6e2
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 19:36:30 +0000

    more verbose error messaging

--- testing.sh
+++ testing.sh
@@ -4,7 +4,15 @@ docker build -t ninthgrimmercury/needlessbeta . &&
     docker build -t freakygamma/needlessbeta test &&
     docker run --interactive --tty  --privileged --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro -p 23628:80 freakygamma/needlessbeta &&
     sleep 10s &&
-    curl http://127.0.0.1:23628/index.html &&
+    if curl http://127.0.0.1:23628 > /dev/null 2>&1
+    then
+	echo the httpd service is working &&
+	    true
+    else
+	echo this httpd service is not working &&
+	    exit 64 &&
+	    true
+    fi &&
     docker rmi --force ninthgrimmercury/needlessbeta &&
     docker rmi --force freakygamma/needlessbeta &&
     true
